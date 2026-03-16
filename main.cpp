8a21a4e97abcd597d492c8f40f1379fcd33bb498
Owen Knizak <138807889+AwokenOwen@users.noreply.github.com>
2026-03-16 14:03:39 -0400

    Edited CMakeList to work with windows and linux

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "GameManager.h"
+#include "WorldManager.h"
 
 int main() {
     Game.initialize();
+
+    World.getActiveScene()->loadDefaultSkybox();
 
     Game.run();
     Game.terminate();
