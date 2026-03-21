d44cf9cb7f99549f13ce1b58d23d08580f2fe365
Owen Knizak <138807889+AwokenOwen@users.noreply.github.com>
2026-03-21 14:15:09 -0400

    fixed assimp again

--- main.cpp
+++ main.cpp
@@ -2,12 +2,36 @@
 #include "WorldManager.h"
 #include "WindowManager.h"
 #include "GUI-Classes/GuiObject.h"
+#include "include/cef_app.h"
+#include <filesystem>
 
-int main() {
+class AwokenApp : public CefApp {
+    IMPLEMENT_REFCOUNTING(AwokenApp);
+};
+
+int main(const int argc, char* argv[]) {
+    CefMainArgs args(argc, argv);
+    CefRefPtr<AwokenApp> pApp = new AwokenApp();
+    int exit_code = CefExecuteProcess(args, pApp, nullptr);
+    if (exit_code >= 0) {
+        return exit_code;  // This IS the subprocess, exit immediately
+    }
+
+    CefSettings settings;
+
+    // FIX THIS OMG YOU NEED TO FIX THIS
+    std::filesystem::path cefLibDir = "/home/awokenowen/GitHub/CEF_Adventures/CEF/lib/linux";
+
+    CefString(&settings.resources_dir_path) = cefLibDir.string();
+    CefString(&settings.locales_dir_path)   = (cefLibDir / "locales").string();
+    CefString(&settings.cache_path) = (cefLibDir / "cache").string();
+
+    CefInitialize(args, settings, pApp, nullptr);
+
     Game.initialize();
     World.getActiveScene()->loadDefaultSkybox();
 
-    auto* a = new GuiObject(vec2(0.0f,0.0f), vec2(0.5f,1.0f));
+    //auto* a = new GuiObject(vec2(0.0f,0.0f), vec2(1.0f,1.0f));
 
     Game.run();
     Game.terminate();
