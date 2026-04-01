fa0948e14fcfb269cf5ef6c022c17facdd9548d7
Owen Knizak <138807889+AwokenOwen@users.noreply.github.com>
2026-03-31 20:49:32 -0400

    YIPEEE

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include "GameManager.h"
+#include "Scene.h"
+#include "Camera.h"
+#include "DirectionalLight.h"
+#include "Material.h"
+#include "MeshRenderer.h"
+#include "test_classes/MovementComponent.h"
 
 int main(const int argc, char* argv[]) {
     Game.initialize();
+    Game.getActiveScene()->loadDefaultSkybox();
+
+    new DirectionalLight;
+
+    Game.getActiveScene()->getCamera()->addComponent<MovementComponent>();
+
+    // IBL Testing
+    const auto sphere = new Object();
+    sphere->addComponent<MeshRenderer>()->loadModel("assets/defaultAssets/Models/sphere.fbx");
+    sphere->getComponent<MeshRenderer>()->getMaterials()[0]->setShaderProgram(
+        "assets/Shaders/smooth_sphere.vert",
+        "assets/Shaders/default_IBL.frag"
+    );
+    sphere->setLocalPosition(vec3(0,0,5));
+
     Game.run();
     Game.terminate();
 }
