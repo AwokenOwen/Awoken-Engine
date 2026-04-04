d8ed3b03b1ea625994eda021dd8cd9f7d47cd156
Owen Knizak <138807889+AwokenOwen@users.noreply.github.com>
2026-04-03 20:22:25 -0400

    set up for collision

--- main.cpp
+++ main.cpp
@@ -17,28 +17,16 @@ int main(const int argc, char* argv[]) {
 
     new FPSDisplay;
 
-    const auto goldMaterial = new Material();
-    goldMaterial->setShaderProgram("assets/Shaders/smooth_sphere.vert", "assets/Shaders/TexturedAlbedoMetallicRoughness.frag");
-    goldMaterial->addTexture("assets/Textures/Gold/sphere_DefaultMaterial_BaseColor.png");
-    goldMaterial->addTexture("assets/Textures/Gold/sphere_DefaultMaterial_Metallic.png");
-    goldMaterial->addTexture("assets/Textures/Gold/sphere_DefaultMaterial_Roughness.png");
+    const auto terracottaMaterial = new Material();
+    terracottaMaterial->setShaderProgram("assets/defaultAssets/Shaders/default.vert", "assets/Shaders/TexturedAlbedoMetallicRoughness.frag");
+    terracottaMaterial->addTexture("assets/Textures/Terracotta/sphere_DefaultMaterial_BaseColor.png");
+    terracottaMaterial->addTexture("assets/Textures/Terracotta/sphere_DefaultMaterial_Metallic.png");
+    terracottaMaterial->addTexture("assets/Textures/Terracotta/sphere_DefaultMaterial_Roughness.png");
 
     const auto a = new Object;
-    a->setLocalPosition(vec3(0.0,0.0,5.0));
-    a->addComponent<MeshRenderer>()->loadModel("assets/defaultAssets/Models/sphere.fbx");
-    a->getComponent<MeshRenderer>()->setMaterial(goldMaterial);
-
-    const auto barkMaterial = new Material();
-    barkMaterial->setShaderProgram("assets/Shaders/smooth_sphere.vert", "assets/Shaders/Textured_Albeto_Normal_Metalic_Roughness.frag");
-    barkMaterial->addTexture("assets/Textures/WoodBark/sphere_DefaultMaterial_BaseColor.png");
-    barkMaterial->addTexture("assets/Textures/WoodBark/sphere_DefaultMaterial_Normal.png");
-    barkMaterial->addTexture("assets/Textures/WoodBark/sphere_DefaultMaterial_Metallic.png");
-    barkMaterial->addTexture("assets/Textures/WoodBark/sphere_DefaultMaterial_Roughness.png");
-
-    const auto b = new Object;
-    b->setLocalPosition(vec3(-2.0,0.0,5.0));
-    b->addComponent<MeshRenderer>()->loadModel("assets/defaultAssets/Models/sphere.fbx");
-    b->getComponent<MeshRenderer>()->setMaterial(barkMaterial);
+    a->setLocalPosition(vec3(0.0,-1.0,0.0));
+    a->addComponent<MeshRenderer>()->loadModel("assets/defaultAssets/Models/plane.fbx");
+    a->getComponent<MeshRenderer>()->setMaterial(terracottaMaterial);
 
     Game.run();
     Game.terminate();
