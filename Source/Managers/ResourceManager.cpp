f7c926fb0a3ad6d5dea08db7a1215ab813f692bb
Owen Knizak <138807889+AwokenOwen@users.noreply.github.com>
2026-06-06 13:24:10 -0400

    Update ResourceManager.cpp

--- Source/Managers/ResourceManager.cpp
+++ Source/Managers/ResourceManager.cpp
@@ -598,7 +598,7 @@ FrameBuffer ResourceManager::makeFramebuffer(const std::string& name, int width,
     // create a color attachment texture
     glGenTextures(1, &frameBuffer.m_colorBuffer);
     glBindTexture(GL_TEXTURE_2D, frameBuffer.m_colorBuffer);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, frameBuffer.m_colorBuffer, 0);
