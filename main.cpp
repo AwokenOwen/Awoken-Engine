274719b806fb296a58b12d57ac1f0e89857c4324
Owen Knizak <138807889+AwokenOwen@users.noreply.github.com>
2026-06-11 07:39:40 -0400

    fixed scene file to have reflection map

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[])
     // Make a name
     // All the objects with their components turned into JSON
 
-    auto scene = new Scene("testScene");
+    /*auto scene = new Scene("testScene");
 
     auto cube = new Object();
     cube->addComponent<ModelRendererComponent>();
@@ -35,7 +35,7 @@ int main(int argc, char* argv[])
 
     delete cube;
     delete scene;
-    delete camera;
+    delete camera;*/
 
     // Then Alter gameInit.json to load the scene
     // Primary is the first scene that will be loaded at start
